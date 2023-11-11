06278f3941e0bccbd6b8a85caa501be64bef3001
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-11 17:13:49 +0530

    Auto push

---
+++ mutable-infra/env-dev/dev-backend.tfvars
@@ -0,0 +1,3 @@
+bucket  = "b55-terraform-bucket"
+key     = "mutable/cart/dev/terraform.tfstate"
+region  = "us-east-1" 
---
+++ mutable-infra/main.tf
@@ -0,0 +1,8 @@
+module "cart" {
+    source               = "./vendor/modules/app"
+    ENV                  = var.ENV  
+    COMPONENT            = var.COMPONENT
+   
+
+}
+
