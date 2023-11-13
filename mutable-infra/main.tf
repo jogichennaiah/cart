26ce94919d08749e2b2de5a13f12a2cbf6b99e28
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-13 17:17:26 +0530

    Auto push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -9,5 +9,6 @@ module "cart" {
     APP_PORT             = var.APP_PORT
     APP_VERSION          = var.APP_VERSION
     INTERNAL             = var.INTERNAL
+    PRIVATE_ALB_ARN      = var.PRIVATE_ALB_ARN
 }
 
